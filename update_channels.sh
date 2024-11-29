6db53a521412a109b653ccfa192bccb158a7aed0
Fernando Ruiz <fernandoruizescobar00@gmail.com>
2024-11-29 12:44:54 +0000

    Update channels

--- update_channels.sh
+++ update_channels.sh
@@ -1,3 +1,5 @@
+eval "$(ssh-agent -s)" > /dev/null
+ssh-add -k /home/fru1z/.ssh/github.pem > /dev/null 
 cd /mnt/disk2/live-tv &&
 curl https://raw.githubusercontent.com/hazofaifo/channels/main/channel.m3u | sed 's/192.168.1.50:6878/192.168.5.50:6870/g' > acestream-1.m3u && cat acestream-1.m3u | sed 's/192.168.5.50:6870/192.168.5.50:6875/g' > acestream-2.m3u;
 if [[ "$(git status | grep -o clean)" != clean ]];then 
